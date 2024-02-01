0cf55e9e31a78bd15a2535c5d03c8e82df0ad365
João Darwin <joaodarwin.ip22@gmail.com>
2024-02-01 16:55:39 -0300

    feat: Adicionar chamada a função de conectar ao mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { config } from "dotenv";
 import app from "./app";
 import Logger from "./config/logger";
+import connectToMongoDb from "./database/mongo";
 
 config();
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
+   connectToMongoDb();
    Logger.info(`Application runing on port ${port}`);
 })
