6302511cff02db62032b7479ff0e139b9db1a2e3
João Darwin <joaodarwin.ip22@gmail.com>
2024-03-01 07:39:45 -0300

    feat: Adicionar middleware em endpoints que requerem authenticação

--- src/routers/UserRouters.ts
+++ src/routers/UserRouters.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
+import { AuthMiddleware } from "../middlewares/auth";
 
 const userRouters = Router();
 
 userRouters.post("/", UserController.create);
-userRouters.delete("/:id", UserController.remove);
+userRouters.delete("/:id", AuthMiddleware, UserController.remove);
 
 export default userRouters;
