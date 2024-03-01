96ee4657b43ce186f37398d34c641f88e379449b
João Darwin <joaodarwin.ip22@gmail.com>
2024-03-01 07:11:20 -0300

    feat: Adicionar lógica no service para remoção do usuário

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,12 +1,29 @@
 import IUserRequest from "../interfaces/dtos/request/IUserRequest";
 import User from "../model/User";
+import { createHashPassword } from "../util/bcrypt";
 
 const createUser = async (user: IUserRequest) => {
     try {
+        user.password = await createHashPassword(user.password);
+
         return await User.create(user);
     } catch (error) {
         throw error;
     }
 }
 
-export { createUser };
+const deleteUser = async (id: String) => {
+    try {
+        const user = await User.findById(id);
+
+        if (user) {
+            await user.deleteOne();
+        } else {
+            throw "User not found!"
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+export { createUser, deleteUser };
