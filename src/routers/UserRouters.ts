aa1b06a0bf337514db956fae8f16a402f705e870
João Darwin <joaodarwin.ip22@gmail.com>
2024-03-01 07:11:45 -0300

    feat: Configurar endpoint nas rotas de Users

--- src/routers/UserRouters.ts
+++ src/routers/UserRouters.ts
@@ -4,5 +4,6 @@ import UserController from "../controllers/UserController";
 const userRouters = Router();
 
 userRouters.post("/", UserController.create);
+userRouters.delete("/:id", UserController.remove);
 
 export default userRouters;
