fd7a93a3314ab5f7c835f39400b29377de5ddaef
João Darwin <joaodarwin.ip22@gmail.com>
2024-03-01 07:56:51 -0300

    refactor: Mover para pacote, controller do User

--- src/controllers/User/UserController.ts
+++ src/controllers/User/UserController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import IUserRequest from "../interfaces/dtos/request/IUserRequest";
-import { createUser, deleteUser, findUserById } from "../services/UserService";
+import IUserRequest from "../../interfaces/dtos/request/IUserRequest";
+import { createUser, deleteUser, findUserById } from "../../services/UserService";
 
 const create = async (req: Request, res: Response) => {
     try {
